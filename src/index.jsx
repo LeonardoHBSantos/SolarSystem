6592d8706aed4bac8f5dff407b02db3ca5a63a6a
LeonardoHBSantos <leosantos1720@gmail.com>
2023-03-31 08:08:44 -0300

    Criando os components Header, Main e Footer

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Main />
+    <Footer />
   </React.StrictMode>,
 )
