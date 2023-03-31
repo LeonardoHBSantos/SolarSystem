341c10667093eb8f1acf50b0d9ce08fd3fe8319a
LeonardoHBSantos <leosantos1720@gmail.com>
2023-03-31 07:53:44 -0300

    Iniciando o Projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Teste</h1>
+      </header>
+    )
+  }
+}
+
+export default App
